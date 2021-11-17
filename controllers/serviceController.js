dfe247b9c4a0a1ce3e36f701f3a7cc427815c4ea
Rahardirizky <rahardirizky@gmail.com>
2021-11-17 20:05:34 +0700

    add try catch in code for handle error

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -2,19 +2,23 @@ const { Service } = require("../models");
 
 class ServiceController {
   static async create(req, res, next) {
-    const { name, fee } = req.body;
+    try {
+      const { name, fee } = req.body;
 
-    const response =  await Service.findAll({ where: { name } });
+      const response = await Service.findAll({ where: { name } });
 
-    if (response.length) {
-      next({
-        status: 409,
-        msg: `Service with name: ${name} are already exists`,
-      });
-    } else {
-      const service = await Service.create({ name, fee });
+      if (response.length) {
+        next({
+          status: 409,
+          msg: `Service with name: ${name} are already exists`,
+        });
+      } else {
+        const service = await Service.create({ name, fee });
 
-      res.status(201).json(service);
+        res.status(201).json(service);
+      }
+    } catch (error) {
+      next(error)
     }
   }
 }
