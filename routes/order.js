28ac9150049d795a44cf91dfb4027a3eb2caf71c
Rahardirizky <rahardirizky@gmail.com>
2021-11-18 02:44:34 +0700

    create routes and methods to get total fee

--- routes/order.js
+++ routes/order.js
@@ -4,6 +4,7 @@ const router = require("express").Router();
 
 router.post("/", OrderController.create);
 router.get("/", OrderController.read);
+router.get("/total", OrderController.getTotalFee)
 router.put("/:id", authorization, OrderController.edit);
 
 module.exports = router;
