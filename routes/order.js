0cdc28b11f8efa34713bbb90884a3fc14f515d43
Rahardirizky <rahardirizky@gmail.com>
2021-11-18 03:29:59 +0700

    order routes update endpoint from get to post

--- routes/order.js
+++ routes/order.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 
 router.post("/", OrderController.create);
 router.get("/", OrderController.read);
-router.get("/total", OrderController.getTotalFee)
+router.post("/total", OrderController.getTotalFee)
 router.put("/:id", authorization, OrderController.edit);
 
 module.exports = router;
