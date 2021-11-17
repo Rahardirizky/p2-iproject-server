721460ea07db08e055bd3f8582f8c048c7a9c0f7
Rahardirizky <rahardirizky@gmail.com>
2021-11-17 19:21:15 +0700

    create routes for registration

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 const express = require('express')
-// const errorHandler = require('./middlewares/errorHandler')
+const errorHandler = require('./middlewares/errorHandler')
 const app = express()
-// const routes = require('./routes')
+const routes = require('./routes')
 const cors = require('cors')
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use(cors())
 
-// app.use('/', routes)
-// app.use(errorHandler)
+app.use('/', routes)
+app.use(errorHandler)
 app.listen(process.env.PORT, () => console.log(`app is running in ${process.env.PORT}`))
