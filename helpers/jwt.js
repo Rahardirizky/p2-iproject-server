cd102471221ec22cf57c09a49deb5a9b8752a7b8
Rahardirizky <rahardirizky@gmail.com>
2021-11-17 19:45:38 +0700

    create helper for token verifying

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -4,6 +4,9 @@ const getToken = (payload) => {
   return jwt.sign(payload, process.env.SECRET)
 }
 
+const verifyToken = (token) => jwt.verify(token, process.env.SECRET)
+
 module.exports = {
-  getToken
+  getToken,
+  verifyToken
 }
