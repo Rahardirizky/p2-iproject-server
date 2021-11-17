fac18f584fc060b3019e9414a58bb1590d295617
Rahardirizky <rahardirizky@gmail.com>
2021-11-17 15:09:55 +0700

    finisihing setup

---
+++ app.js
@@ -0,0 +1,17 @@
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+// const errorHandler = require('./middlewares/errorHandler')
+const app = express()
+// const routes = require('./routes')
+const cors = require('cors')
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+app.use(cors())
+
+// app.use('/', routes)
+// app.use(errorHandler)
+app.listen(process.env.PORT, () => console.log(`app is running in ${process.env.PORT}`))
